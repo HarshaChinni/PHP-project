082f1f7367d3274e565aa8c0189d1361ea764ea9
HarshaChinni <sriharsha.ch30@gmail.com>
2018-11-30 12:27:20 -0700

    Connceted to DB

--- php/update-food-details.php
+++ php/update-food-details.php
@@ -41,44 +41,82 @@
         </div>
         <div class="container">
             <div class="row">
-                <div class="col-md-12">
-                    <form class="form" action="./food-items-list.php">
-                        <span class="form-title">
-                            Update food item
-                        </span>
-                        <div class="div-input">
-                            <span class="label-input100">Food Name</span>
-                            <input class="input100" type="text" name="name" value="">
-                        </div>
-                        <div class="div-input">
-                            <span class="label-input100">Food Type</span>
-                            <input class="input100" type="text" name="type" value="Indian">
-                        </div>
-                        <div class="div-input">
-                            <span class="label-input100">Price</span>
-                            <input class="input100" type="text" name="price" value="">
-                        </div>
-                        <div class="div-input">
-                            <span class="label-input100">Description</span>
-                            <input class="input100" type="text" name="description" value="Spicy food">
-                        </div>
-                        <div class="div-input">
-                            <span class="label-input100">Ingredients</span>
-                            <textarea class="input100" name="ingredients">
-                                Mushrooms, Olives, Onions, Green  bell peppers and Fresh tomatoes.
-                            </textarea>
-                        </div>
+              <?php
+                require_once './db_connection.php';
+                $conn = new mysqli($hn, $un, $pw, $db);
+                // print_r($conn);
+                $food_id = $_GET['food_id'];
+                $query = "SELECT * FROM food_item WHERE food_item.item_id = $food_id";
+                // print_r($query);
+                $result = $conn->query($query);
+                // print_r($result);
+                $rowCount = $result->num_rows;
+                for($loop=0; $loop< $rowCount; $loop++){
+                  $result->data_seek($loop);
+                  $row = $result->fetch_array(MYSQLI_NUM);
+                  print_r($row);
+                  echo <<<_END
+                      <div class="col-md-12">
+                          <form class="form" action="./update-food-details.php?food_id=$food_id" method="post">
+                              <span class="form-title">
+                                  Update food item
+                              </span>
+                              <div class="div-input">
+                                  <span class="label-input100">Food Name</span>
+                                  <input class="input100" type="text" name="name" value="$row[1]">
+                              </div>
+                              <div class="div-input">
+                                  <span class="label-input100">Food Type</span>
+                                  <input class="input100" type="text" name="type" value="$row[2]">
+                              </div>
+                              <div class="div-input">
+                                  <span class="label-input100">Price</span>
+                                  <input class="input100" type="text" name="price" value="$row[3]">
+                              </div>
+                              <div class="div-input">
+                                  <span class="label-input100">Description</span>
+                                  <input class="input100" type="text" name="description" value="$row[5]">
+                              </div>
+                              <div class="div-input">
+                                  <span class="label-input100">Ingredients</span>
+                                  <textarea class="input100" name="ingredients">
+                                    $row[6]
+                                  </textarea>
+                              </div>
+                              <div class="div-input">
+                                  <span class="label-input100">Food Image</span>
+                                  <input class="input100" type="text" name="image" value="$row[4]">
+                              </div>
 
-                        <div class="container-contact100-form-btn">
-                            <button class="contact100-form-btn">
-                                Update
-                            </button>
-                        </div>
-                    </form>
-                </div>
+                              <div class="container-contact100-form-btn">
+                                  <button type="submit" class="contact100-form-btn">
+                                      Update food
+                                  </button>
+                              </div>
+                          </form>
+                      </div>
+_END;
+                }
+
+                  if(isset($_POST)){
+                    echo '<br>';
+                    print_r($_POST);
+                    $name = $_POST['name'];
+                    $type = $_POST['type'];
+                    $price = $_POST['price'];
+                    $description = $_POST['description'];
+                    $ingredients = $_POST['ingredients'];
+                    $image = $_POST['image'];
+
+                    $query = "UPDATE food_item SET item_name = '$name', item_type='$type', price='$price', item_image='$image', description='$description', ingredients='$ingredients' WHERE item_id = $food_id";
+                    echo '<br>';
+                    // print_r($query);
+                    $result = $conn->query($query);
+                    // print_r($result);
+                  }
+
+              ?>
             </div>
         </div>
     </body>
-
-
 </html>
